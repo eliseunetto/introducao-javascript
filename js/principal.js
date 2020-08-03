eb55f8adfd630b174ec4b254948bc50a0e86cba9
eliseunetto <eliseunetto@hotmail.com>
2020-08-03 19:21:11 -0300

    query selector
    Utilizando a variável document junto com o queryselector para captuar conteúdos do nosso HTML

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+ console.log("Fui carregado de um arquivo externo");
+ 
+ var titulo = document.querySelector(".titulo");
+
+        // console.log(document.querySelector("h1").textContent);
+        console.log(titulo.textContent);
+        titulo.textContent = "Aparecida Nutricionista";
