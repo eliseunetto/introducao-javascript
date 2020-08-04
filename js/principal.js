66f73bc34e031c3338f6bf4c11a45634b199cd1e
eliseunetto <eliseunetto@hotmail.com>
2020-08-04 14:39:20 -0300

    Buscando dados do paciente
    Buscando dados do paciente e calculando o IMC e imprimindo as informações capturadas no console

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,22 @@
- console.log("Fui carregado de um arquivo externo");
+// console.log("Fui carregado de um arquivo externo");
  
  var titulo = document.querySelector(".titulo");
 
         // console.log(document.querySelector("h1").textContent);
         console.log(titulo.textContent);
         titulo.textContent = "Aparecida Nutricionista";
+
+        var paciente = document.querySelector("#primeiro-paciente");
+
+        var tdPeso = paciente.querySelector(".info-peso");
+        var peso = tdPeso.textContent;
+        
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
+
+        var imc = peso / (altura * altura);
+
+        console.log(paciente); // tr
+        console.log(tdPeso); // td que tem o peso
+        console.log(peso); // conteúdo(valor) da td
+        console.log(imc); // resultado do IMC
