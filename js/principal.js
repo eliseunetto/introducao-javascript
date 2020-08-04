18362b65420336a1838f1ab1cdb06dc4bc130d4b
eliseunetto <eliseunetto@hotmail.com>
2020-08-04 15:01:21 -0300

    Calculando e exibindo o IMC corretamente

--- js/principal.js
+++ js/principal.js
@@ -10,13 +10,12 @@
 
         var tdPeso = paciente.querySelector(".info-peso");
         var peso = tdPeso.textContent;
-        
+
         var tdAltura = paciente.querySelector(".info-altura");
         var altura = tdAltura.textContent;
 
+        var tdIMC = paciente.querySelector(".info-imc");
+
         var imc = peso / (altura * altura);
 
-        console.log(paciente); // tr
-        console.log(tdPeso); // td que tem o peso
-        console.log(peso); // conteúdo(valor) da td
-        console.log(imc); // resultado do IMC
+       tdIMC.textContent = imc;
