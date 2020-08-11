3059e7109bc810e96404ca30f494de9ad0984835
eliseunetto <eliseunetto@hotmail.com>
2020-08-10 22:16:51 -0300

    update validações e msg ERRO

--- js/form.js
+++ js/form.js
@@ -11,12 +11,13 @@ botaoAddPaciente.addEventListener("click", function(event) {
     var trPaciente = montaTr(paciente);
     
     var erros = validaPaciente(paciente);
-
+    
+    console.log(erros);
     if(erros.length > 0){
-        var seletor = document.querySelector("#mensagem-erro");
-        seletor.textContent = erros;
-        form.reset();
-        erros = "";
+        exibeMendagemDeErro(erros);
+        if(erros.length == 0){
+            form.reset();
+        }
         return;
     }
 
@@ -27,7 +28,7 @@ botaoAddPaciente.addEventListener("click", function(event) {
 
     form.reset();
 
-   
+    limpaMsgErro();
 });
 
 function obtemPacienteDoFormulario(form){
@@ -74,8 +75,29 @@ function validaPaciente(paciente) {
 
     var erros = [];
 
+    if(paciente.nome.length == 0) erros.push("Nome não pode ficar em branco");
+    if(paciente.peso.length == 0) erros.push("Peso não pode ficar em branco");
+    if(paciente.altura.length == 0) erros.push("Altura não pode ficar em branco");
+    if(paciente.gordura.length == 0) erros.push("% Gordura não pode ficar em branco");
+
     if(!validaPeso(paciente.peso)) erros.push(" Peso inválido");
     if(!validaAltura(paciente.altura)) erros.push(" Altura inválida");
 
     return erros;
+}
+
+function exibeMendagemDeErro(erros){
+    var ul = document.querySelector("#mensagens-erros");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+function limpaMsgErro(){
+    var mensagensErro = document.querySelector("#mensagens-erros");
+    mensagensErro.innerHTML = "";
 }
