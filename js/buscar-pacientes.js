2a4d5a8f8c836de745a30a08cdced4e246626895
eliseuentto <eliseunetto@hotmail.com>
2020-09-28 15:48:15 -0300

    add botão Buaca Paciente

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,5 @@
+var btnBuscarPacientes = document.querySelector("#buscar-pacientes");
+
+btnBuscarPacientes.addEventListener("click", function(){
+    console.log("BNT Buscar Paciente CLICADO!");
+});
